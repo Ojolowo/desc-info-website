d3d26ab85dd3e02d11e4e0526beb2f2aab386bfa
Ojolowo <tekeramedia@gmail.com>
2022-01-02 00:02:50 +0100

    fix type issues

--- theme/global.ts
+++ theme/global.ts
@@ -1,6 +1,6 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle, DefaultTheme } from "styled-components";
 
-export const GlobalStyles = createGlobalStyle`
+export const GlobalStyles = createGlobalStyle<{ theme: DefaultTheme }>`
   body {
     background: ${({ theme }) => theme.primaryBackground};
     color: ${({ theme }) => theme.secondaryText};
@@ -8,3 +8,21 @@ font-family: Poppins, sans-serif;
     transition: all 0.50s linear;
   }
 `;
+
+declare module "styled-components" {
+   export interface DefaultTheme {
+      link: string;
+      primaryBackground: string;
+      secondaryText: string;
+      secondaryBackground: string;
+      primaryText: string;
+      footerBackground: string;
+      textBrief: string;
+      nav: string;
+      statsBg: string;
+      statsText: string;
+      toggler: string;
+      card: string;
+      testimonials: string;
+   }
+}
